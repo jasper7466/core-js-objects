49faaffc7df5c9d40cd77a7dc2575740597b62f4
Tim <jasper7466@yandex.ru>
2024-02-29 01:08:19 +0500

    feat: solve "fromJSON" problem

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -241,8 +241,12 @@ function getJSON(obj) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const instance = JSON.parse(json);
+
+  Object.setPrototypeOf(instance, proto);
+
+  return instance;
 }
 
 /**
